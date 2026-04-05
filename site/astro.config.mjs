2b432e4a681a126e44fefb33c01ec7d4447c3956
Kumar Anirudha <mail@anirudha.dev>
2026-04-05 20:36:54 +0530

    fix: trailing slash in baseurl

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -7,7 +7,7 @@ const isProd = process.env.NODE_ENV === 'production';
 // https://astro.build/config
 export default defineConfig({
   site: isProd ? 'https://nullorder.github.io' : undefined,
-  base: isProd ? '/agenthub' : '/',
+  base: isProd ? '/agenthub/' : '/',
   server: {
     port: 3000
   },
